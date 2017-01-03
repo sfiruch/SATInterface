3c57378ff447b59ee31f04cfdec0850c7ee3dfc5
Simon Felix <de@iru.ch>
2017-01-03 22:34:55 +0100

    updated examples to reflect "documentation"

--- SATInterface/readme.txt
+++ SATInterface/readme.txt
@@ -1,8 +1,13 @@
-﻿//SATInterface is a library to formulate SAT problems in .NET 
+﻿// SATInterface is a library to formulate SAT problems in .NET 
+// - https://github.com/deiruch/SATInterface
 //
-//By default the bundeled excellent CryptoMiniSAT solver is used as backend, but any solver
-//that supports DIMACS can be used (e.g. Lingeling, RISS, MiniSAT, ...). CryptoMiniSAT
-//is MIT licensed and available from https://github.com/msoos/cryptominisat
+//
+// By default the bundeled excellent CryptoMiniSAT solver is used as backend, but any solver
+// that supports DIMACS can be used (e.g. Lingeling, RISS, MiniSAT, ...). CryptoMiniSAT
+// is MIT licensed and available from https://github.com/msoos/cryptominisat
+//
+//
+// Here's a usage example: Sudoku
 
 var m = new Model();
 var v = new BoolExpr[9, 9, 9];
@@ -11,18 +16,22 @@ for (var y = 0; y < 9; y++)
         for (var n = 0; n < 9; n++)
             v[x, y, n] = new BoolVar(m);
 
+//assign one number to each cell
 for (var y = 0; y < 9; y++)
     for (var x = 0; x < 9; x++)
         m.AddConstr(m.ExactlyOneOf(Enumerable.Range(0,9).Select(n => v[x,y,n])));
 
+//each number occurs once per row
 for (var y = 0; y < 9; y++)
     for (var n = 0; n < 9; n++)
         m.AddConstr(m.ExactlyOneOf(Enumerable.Range(0, 9).Select(x => v[x, y, n])));
 
+//each number occurs once per column
 for (var x = 0; x < 9; x++)
     for (var n = 0; n < 9; n++)
         m.AddConstr(m.ExactlyOneOf(Enumerable.Range(0, 9).Select(y => v[x, y, n])));
 
+//each number occurs once per 3x3 block
 for (var n = 0; n < 9; n++)
     for(var y=0;y<9;y+=3)
         for (var x = 0; x < 9; x += 3)
@@ -32,12 +41,12 @@ for (var n = 0; n < 9; n++)
 
 m.Solve();
 
-
-for(var y=0;y<9;y++)
-{
-    for (var x = 0; x < 9; x++)
-        for (var n = 0; n < 9; n++)
-            if (v[x, y, n].X)
-                Console.Write($" {n+1}");
-    Console.WriteLine();
-}
+if(m.IsSatisfiable)
+    for(var y=0;y<9;y++)
+    {
+        for (var x = 0; x < 9; x++)
+            for (var n = 0; n < 9; n++)
+                if (v[x, y, n].X)
+                    Console.Write($" {n+1}");
+        Console.WriteLine();
+    }
--- Sudoku/Program.cs
+++ Sudoku/Program.cs
@@ -18,52 +18,57 @@ namespace Sudoku
                     for (var n = 0; n < 9; n++)
                         v[x, y, n] = new BoolVar(m);
 
-            //http://www.telegraph.co.uk/news/science/science-news/9359579/Worlds-hardest-sudoku-can-you-crack-it.html
-
-            var sudoku =    "8........" +
-                            "..36....." +
-                            ".7..9.2.." +
-                            ".5...7..." +
-                            "....457.." +
-                            "...1...3." +
-                            "..1....68" +
-                            "..85...1." +
-                            ".9....4..";
-
+            //According to http://www.telegraph.co.uk/news/science/science-news/9359579/Worlds-hardest-sudoku-can-you-crack-it.html
+            //this is the "World's hardest Sudoku"...
+            var sudoku = 
+                "8........" +
+                "..36....." +
+                ".7..9.2.." +
+                ".5...7..." +
+                "....457.." +
+                "...1...3." +
+                "..1....68" +
+                "..85...1." +
+                ".9....4..";
             for (var y = 0; y < 9; y++)
                 for (var x = 0; x < 9; x++)
                     if (sudoku[y * 9 + x] != '.')
                         v[x, y, sudoku[y * 9 + x] - '1'] = true;
 
+            //assign one number to each cell
             for (var y = 0; y < 9; y++)
                 for (var x = 0; x < 9; x++)
-                    m.AddConstr(m.ExactlyOneOf(Enumerable.Range(0,9).Select(n => v[x,y,n])));
+                    m.AddConstr(m.ExactlyOneOf(Enumerable.Range(0, 9).Select(n => v[x, y, n])));
 
+            //each number occurs once per row
             for (var y = 0; y < 9; y++)
                 for (var n = 0; n < 9; n++)
                     m.AddConstr(m.ExactlyOneOf(Enumerable.Range(0, 9).Select(x => v[x, y, n])));
 
+            //each number occurs once per column
             for (var x = 0; x < 9; x++)
                 for (var n = 0; n < 9; n++)
                     m.AddConstr(m.ExactlyOneOf(Enumerable.Range(0, 9).Select(y => v[x, y, n])));
 
+            //each number occurs once per 3x3 block
             for (var n = 0; n < 9; n++)
-                for(var y=0;y<9;y+=3)
+                for (var y = 0; y < 9; y += 3)
                     for (var x = 0; x < 9; x += 3)
-                        m.AddConstr(m.ExactlyOneOf(v[x+0, y+0, n], v[x + 1, y + 0, n], v[x + 2, y + 0, n],
+                        m.AddConstr(m.ExactlyOneOf(v[x + 0, y + 0, n], v[x + 1, y + 0, n], v[x + 2, y + 0, n],
                             v[x + 0, y + 1, n], v[x + 1, y + 1, n], v[x + 2, y + 1, n],
                             v[x + 0, y + 2, n], v[x + 1, y + 2, n], v[x + 2, y + 2, n]));
 
             m.Solve();
 
-            for(var y=0;y<9;y++)
-            {
-                for (var x = 0; x < 9; x++)
-                    for (var n = 0; n < 9; n++)
-                        if (v[x, y, n].X)
-                            Console.Write($" {n+1}");
-                Console.WriteLine();
-            }
+            if (m.IsSatisfiable)
+                for (var y = 0; y < 9; y++)
+                {
+                    for (var x = 0; x < 9; x++)
+                        for (var n = 0; n < 9; n++)
+                            if (v[x, y, n].X)
+                                Console.Write($" {n + 1}");
+                    Console.WriteLine();
+                }
 
             Console.WriteLine("Done");
             Console.ReadLine();
