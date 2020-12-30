abcb87002b55e7cb2412b9b37d17017a7c5ce5d6
Simon Felix <de@iru.ch>
2020-12-30 14:27:49 +0100

    improve documentation

--- SATInterface/Configuration.cs
+++ SATInterface/Configuration.cs
@@ -6,8 +6,19 @@ namespace SATInterface
 {
     public enum OptimizationFocus
     {
+        /// <summary>
+        /// Binary search
+        /// </summary>
         Balanced,
+
+        /// <summary>
+        /// Focus on finding incumbents
+        /// </summary>
         Incumbent,
+
+        /// <summary>
+        /// Focus on proving bounds
+        /// </summary>
         Bound
     }
 
@@ -23,37 +34,44 @@ namespace SATInterface
     {
         /// <summary>
         /// Strategy to solve minimization/maximization problems.
+        /// Default: Balanced
         /// </summary>
         public OptimizationFocus OptimizationFocus = OptimizationFocus.Balanced;
         
         /// <summary>
         /// Verbosity of the solver logging. Set to 0 to disable logging.
+        /// Default: 2
         /// </summary>
         public int Verbosity = 2;
 
         /// <summary>
         /// Bundled SAT solver to use.
+        /// Default: CaDiCaL
         /// </summary>
         public InternalSolver Solver = InternalSolver.CaDiCaL;
 
         /// <summary>
         /// Number of threads the solver may use.
+        /// Default: null
         /// </summary>
         public int? Threads;
 
         /// <summary>
         /// Random seed used by the solver for tie-breaking.
+        /// Default: null
         /// </summary>
         public int? RandomSeed;
 
         /// <summary>
         /// Initial phase of variables.
+        /// Default: null
         /// </summary>
         public bool? InitialPhase;
 
         /// <summary>
         /// Eliminating duplicate subexpressions makes setting up models
         /// more resource intensive, but solving potentially faster.
+        /// Default: false
         /// </summary>
         public bool CommonSubexpressionElimination = false;
 
--- Sudoku/Program.cs
+++ Sudoku/Program.cs
@@ -1,77 +1,63 @@
-﻿using SATInterface;
+﻿// SATInterface is a library to formulate SAT problems in .NET 
+// - https://github.com/deiruch/SATInterface
+//
+// The interface includes these excellent SAT solvers, but any solver
+// that supports DIMACS can be used (e.g. Lingeling, Clasp, RISS, MiniSAT, ...).
+//
+// * CaDiCaL is MIT-licensed and available from https://github.com/arminbiere/cadical
+// * Kissat is MIT-licensed and available from https://github.com/arminbiere/kissat
+// * CryptoMiniSat is MIT-licensed and available from https://github.com/msoos/cryptominisat
+//
+// Here's a usage example: Sudoku
+
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
+using SATInterface;
 
-namespace Sudoku
-{
-    class Program
+using var m = new Model();
+m.Configuration.Solver = InternalSolver.CaDiCaL;
+m.Configuration.Verbosity = 2;
+
+var v = m.AddVars(9, 9, 9);
+
+//fix the first number to 1
+v[0, 0, 0] = true;
+
+//here's alternative way to set the second number
+m.AddConstr(v[1, 0, 1]);
+
+//assign one number to each cell
+for (var y = 0; y < 9; y++)
+    for (var x = 0; x < 9; x++)
+        m.AddConstr(m.Sum(Enumerable.Range(0, 9).Select(n => v[x, y, n])) == 1);
+
+//each number occurs once per row (alternative formulation)
+for (var y = 0; y < 9; y++)
+    for (var n = 0; n < 9; n++)
+        m.AddConstr(m.ExactlyOneOf(Enumerable.Range(0, 9).Select(x => v[x, y, n])));
+
+//each number occurs once per column (configured formulation)
+for (var x = 0; x < 9; x++)
+    for (var n = 0; n < 9; n++)
+        m.AddConstr(m.ExactlyOneOf(Enumerable.Range(0, 9).Select(y => v[x, y, n]), Model.ExactlyOneOfMethod.PairwiseTree));
+
+//each number occurs once per 3x3 block
+for (var n = 0; n < 9; n++)
+    for (var y = 0; y < 9; y += 3)
+        for (var x = 0; x < 9; x += 3)
+            m.AddConstr(m.Sum(
+                v[x + 0, y + 0, n], v[x + 1, y + 0, n], v[x + 2, y + 0, n],
+                v[x + 0, y + 1, n], v[x + 1, y + 1, n], v[x + 2, y + 1, n],
+                v[x + 0, y + 2, n], v[x + 1, y + 2, n], v[x + 2, y + 2, n]) == 1);
+
+m.Solve();
+
+if (m.State == State.Satisfiable)
+    for (var y = 0; y < 9; y++)
     {
-        static void Main(string[] args)
-        {
-            using var m = new Model();
-            var v = m.AddVars(9, 9, 9);
-
-            //According to http://www.telegraph.co.uk/news/science/science-news/9359579/Worlds-hardest-sudoku-can-you-crack-it.html
-            //this is the "World's hardest Sudoku"...
-            var sudoku =
-                "8........" +
-                "..36....." +
-                ".7..9.2.." +
-                ".5...7..." +
-                "....457.." +
-                "...1...3." +
-                "..1....68" +
-                "..85...1." +
-                ".9....4..";
-            for (var y = 0; y < 9; y++)
-                for (var x = 0; x < 9; x++)
-                    if (sudoku[y * 9 + x] != '.')
-                        v[x, y, sudoku[y * 9 + x] - '1'] = true;
-
-            //assign one number to each cell
-            for (var y = 0; y < 9; y++)
-                for (var x = 0; x < 9; x++)
-                    m.AddConstr(m.Sum(Enumerable.Range(0, 9).Select(n => v[x, y, n]))==1);
-
-            //each number occurs once per row
-            for (var y = 0; y < 9; y++)
-                for (var n = 0; n < 9; n++)
-                    m.AddConstr(m.Sum(Enumerable.Range(0, 9).Select(x => v[x, y, n]))==1);
-
-            //each number occurs once per column
-            for (var x = 0; x < 9; x++)
-                for (var n = 0; n < 9; n++)
-                    m.AddConstr(m.Sum(Enumerable.Range(0, 9).Select(y => v[x, y, n])) == 1);
-
-            //each number occurs once per 3x3 block
+        for (var x = 0; x < 9; x++)
             for (var n = 0; n < 9; n++)
-                for (var y = 0; y < 9; y += 3)
-                    for (var x = 0; x < 9; x += 3)
-                        m.AddConstr(m.Sum(v[x + 0, y + 0, n], v[x + 1, y + 0, n], v[x + 2, y + 0, n],
-                            v[x + 0, y + 1, n], v[x + 1, y + 1, n], v[x + 2, y + 1, n],
-                            v[x + 0, y + 2, n], v[x + 1, y + 2, n], v[x + 2, y + 2, n]) == 1);
-
-            m.Configuration.Verbosity = 0;
-            m.EnumerateSolutions(v.Cast<BoolExpr>(),
-                () =>
-                {
-                    for (var y = 0; y < 9; y++)
-                    {
-                        for (var x = 0; x < 9; x++)
-                            for (var n = 0; n < 9; n++)
-                                if (v[x, y, n].X)
-                                    Console.Write($" {n + 1}");
-                        Console.WriteLine();
-                    }
-                    Console.WriteLine();
-                });
-
-            Console.WriteLine("Done");
-            Console.ReadLine();
-        }
+                if (v[x, y, n].X)
+                    Console.Write($" {n + 1}");
+        Console.WriteLine();
     }
-}
